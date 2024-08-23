00f7f8fda424b41b3abd362e077e44083cc44086
Vladislavhgtech <vld.85@inbox.ru>
2024-08-23 16:48:36 +0300

    ПР 1 корректировки по ревью 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,23 +7,24 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         Race race=new Race();
+        Car[] cars=new Car[3];
 
         for (int i = 0; i < 3; i++) {
-            System.out.print("Введите название машины №"+(i+1));
+            System.out.println("Введите название машины №"+(i+1));
             String carName = scanner.next();
-            System.out.print("Введите скорость машины №"+(i+1));
+            System.out.println("Введите скорость машины №"+(i+1));
             int carSpeed=scanner.nextInt();
             while(!(carSpeed > 0 && carSpeed <= 250)) {
                 System.out.println("Неправильная скорость");
-                System.out.print("Введите скорость машины №"+(i+1));
+                System.out.println("Введите скорость машины №"+(i+1));
                 carSpeed  = scanner.nextInt();
             }
 
+            cars[i]=new Car(carName, carSpeed);
 
-            race=new Race(new Car(carName, carSpeed));
         }
 
 
-        race.bestCar();
+        race.bestCar(cars);
     }
 }
--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -1,24 +1,21 @@
 public class Race {
-    private final Car car;
-    private String leader="";
-    private int distance=0;
+    private String leader;
+    private int distance;
 
     public Race() {
-        car=new Car("", 0);
-    }
-    public Race(Car car) {
-        this.car = car;
+        this.leader="";
+        this.distance=0;
     }
 
 
-    public void bestCar () {
-        leader = car.getName();
-        distance=car.getSpeed()*24;
-
-        if (distance < car.getSpeed()*24) {
-            distance = car.getSpeed()*24;
-            leader = car.getName();
+    public void bestCar (Car[] cars) {
+        for (Car car : cars) {
+            if (distance < car.getSpeed()*24) {
+                distance = car.getSpeed()*24;
+                leader= car.getName();
+            }
         }
+
         System.out.println("Самая быстрая машина: " + leader);
     }
 
